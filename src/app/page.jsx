d53c3302e7ad2b02fb77eb17b5be3ba76ead3471
Tom Stuart <tom@yougov.finance>
2024-01-20 22:42:57 +0000

    DecentraIsed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -168,7 +168,7 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
         <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Decentralsed venture capital for the <span className='font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-700/70 via-purple-800 to-pink-700/50'>Internet Computer.</span>
+            Decentralised venture capital for the <span className='font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-700/70 via-purple-800 to-pink-700/50'>Internet Computer.</span>
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             ICVC is a decentralised venture capital fund dedicated to supporting the Internet Computer ecosystem. 
